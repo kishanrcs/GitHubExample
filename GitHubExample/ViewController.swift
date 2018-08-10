421875f5f98429523ccc0fbd172421c8d8754ebb
Magna <kishanrcs>
2018-08-10 14:28:51 +0530

    Added reverse function and printed when view loads

--- GitHubExample/ViewController.swift
+++ GitHubExample/ViewController.swift
@@ -13,8 +13,13 @@ let message = "Hello Git!"
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
+        print(reverse(text: "stressed"))
     }
 
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
